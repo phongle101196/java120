e2ad4a6a724b7126adadbb328bdb155323236c8a
phongle101196 <saymesach@gmail.com>
2025-11-25 15:35:27 +0700

    update message to "Hello you"

--- src/main/java/learnjava/day1/Day1.java
+++ src/main/java/learnjava/day1/Day1.java
@@ -2,6 +2,6 @@ package learnjava.day1;
 
 public class Day1 {
      public static void main(String[] args) {
-          System.out.println("Hello world");
+          System.out.println("Hello you");
      }
 }
